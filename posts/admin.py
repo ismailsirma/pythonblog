67c98631487406d1ca7a2f77856625fbb2b4b38d
ismailsirma <ismailsirma@gmail.com>
2018-04-08 18:59:01 +0300

    Admin panel customizations, filters, more info model info on panel

--- posts/admin.py
+++ posts/admin.py
@@ -3,4 +3,13 @@ from django.contrib import admin
 # Register your models here.
 from posts.models import Post
 
-admin.site.register(Post)
+class PostModelAdmin(admin.ModelAdmin):
+    list_display = ["__unicode__","updated","timestamp"] # adds time info to admin panel model page
+    list_display_links = ["updated"] # add link to info show on admin
+    list_editable = ["title"]
+    list_filter = ["updated","timestamp"] # add filter
+    search_fields = ["title","content"]
+    class Meta:
+        model = Post
+
+admin.site.register(Post, PostModelAdmin)
