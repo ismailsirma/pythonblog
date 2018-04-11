8d58d1d1db286639d4bdb37e62c45731adb13ca1
ismailsirma <ismailsirma@gmail.com>
2018-04-12 01:03:55 +0300

    Templates are added, contexts put into template htmls

--- posts/urls.py
+++ posts/urls.py
@@ -2,11 +2,20 @@ from django.urls import path, include, re_path
 from posts import views
 # from posts import post_view # class based view
 from . import views
+# or adding views from this: extra added, double code here!
+# function based views 
+from .views import (
+    post_list,
+    post_create,
+    post_detail,
+    post_update,
+    post_delete,
+)
 
 urlpatterns = [
     re_path(r'^$', views.post_list), # first url to be matched
     re_path(r'^create/$', views.post_create), # function based view added
-    re_path(r'^detail/$', views.post_detail), #
+    re_path(r'^detail/$', views.post_detail), # "posts.views.post_detail" could be added this way (old way)
     re_path(r'^update/$', views.post_update), #
     re_path(r'^delete/$', views.post_delete), #
 ]
--- posts/views.py
+++ posts/views.py
@@ -5,10 +5,24 @@ def post_create(request):
     return HttpResponse("<h1>Create</h1>")
 
 def post_detail(request):
-    return HttpResponse("<h1>Detail</h1>")
+    context = {
+        "title": "Detail"
+    }
+    return render(request,"index.html",context) # context as an empty dictionary
+    #return HttpResponse("<h1>Detail</h1>")
 
 def post_list(request):
-    return HttpResponse("<h1>List</h1>")
+    if request.user.is_authenticated():
+        context = {
+            "title": "My User List"
+        }
+        #return render(request,"index2.html",context)
+    else :
+        context = {
+            "title": "List"
+        }
+        return render(request,"index.html",context) # context as an empty dictionary
+    #return HttpResponse("<h1>List</h1>")
 
 def post_update(request):
     return HttpResponse("<h1>Update</h1>")
