dc9dc72ce801db31f79a2c57cd7a706811e85ad5
ismailsirma <ismailsirma@gmail.com>
2018-04-14 00:15:24 +0300

    adding querysets to represent model objects in view pages

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
+from .models import Post
+
 # Create your views here.
 def post_create(request):
     return HttpResponse("<h1>Create</h1>")
@@ -12,16 +15,18 @@ def post_detail(request):
     #return HttpResponse("<h1>Detail</h1>")
 
 def post_list(request):
-    if request.user.is_authenticated():
-        context = {
-            "title": "My User List"
-        }
-        #return render(request,"index2.html",context)
-    else :
-        context = {
-            "title": "List"
-        }
-        return render(request,"index.html",context) # context as an empty dictionary
+
+    queryset = Post.objects.all()
+    context = {
+        "object_list":queryset,
+        "title":"List",
+    }
+    # if request.user.is_authenticated():
+    #     context = {"title": "My User List"}
+    #     #return render(request,"index2.html",context)
+    # else:
+    #     context = {"title": "List"}
+    return render(request,"index.html",context) # context as an empty dictionary
     #return HttpResponse("<h1>List</h1>")
 
 def post_update(request):
